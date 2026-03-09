e70b2426dda3534e44c1691ffacf11585b049a51
John Smith <raymx1998@gmail.com>
2026-03-09 20:27:43 +0800

    Code format optimization

--- include/common.h
+++ include/common.h
@@ -1,9 +1,9 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <sys/types.h>
 
 constexpr size_t PAGE_SIZE = 4096;
@@ -18,16 +18,16 @@ constexpr int MAX_TABLES = 5;
 typedef enum : uint8_t { FIELD_INT, FIELD_TEXT } FieldType;
 
 typedef struct {
-    char name[FIELD_NAME_MAX];
-    FieldType type;
-    uint32_t size;
-    uint32_t offset;
+  char name[FIELD_NAME_MAX];
+  FieldType type;
+  uint32_t size;
+  uint32_t offset;
 } Field;
 
 typedef struct {
-    uint32_t num_fields;
-    Field fields[MAX_FIELDS];
-    uint32_t row_size;
+  uint32_t num_fields;
+  Field fields[MAX_FIELDS];
+  uint32_t row_size;
 } Schema;
 
 typedef enum : uint8_t { NODE_INTERNAL, NODE_LEAF } NodeType;
@@ -41,25 +41,33 @@ constexpr size_t IS_ROOT_SIZE = sizeof(uint8_t);
 constexpr size_t IS_ROOT_OFFSET = NODE_TYPE_SIZE;
 constexpr size_t PARENT_POINTER_SIZE = sizeof(uint32_t);
 constexpr size_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
-constexpr size_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
+constexpr size_t COMMON_NODE_HEADER_SIZE =
+    NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
 
 /* Leaf Node Header Layout (num_cells, next_leaf) */
 constexpr size_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
 constexpr size_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
 constexpr size_t LEAF_NODE_NEXT_LEAF_SIZE = sizeof(uint32_t);
-constexpr size_t LEAF_NODE_NEXT_LEAF_OFFSET = LEAF_NODE_NUM_CELLS_OFFSET + LEAF_NODE_NUM_CELLS_SIZE;
-constexpr size_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE + LEAF_NODE_NEXT_LEAF_SIZE;
+constexpr size_t LEAF_NODE_NEXT_LEAF_OFFSET =
+    LEAF_NODE_NUM_CELLS_OFFSET + LEAF_NODE_NUM_CELLS_SIZE;
+constexpr size_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE +
+                                         LEAF_NODE_NUM_CELLS_SIZE +
+                                         LEAF_NODE_NEXT_LEAF_SIZE;
 
 /* Internal Node Header Layout (num_keys, right_child) */
 constexpr size_t INTERNAL_NODE_NUM_KEYS_SIZE = sizeof(uint32_t);
 constexpr size_t INTERNAL_NODE_NUM_KEYS_OFFSET = COMMON_NODE_HEADER_SIZE;
 constexpr size_t INTERNAL_NODE_RIGHT_CHILD_SIZE = sizeof(uint32_t);
-constexpr size_t INTERNAL_NODE_RIGHT_CHILD_OFFSET = INTERNAL_NODE_NUM_KEYS_OFFSET + INTERNAL_NODE_NUM_KEYS_SIZE;
-constexpr size_t INTERNAL_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + INTERNAL_NODE_NUM_KEYS_SIZE + INTERNAL_NODE_RIGHT_CHILD_SIZE;
+constexpr size_t INTERNAL_NODE_RIGHT_CHILD_OFFSET =
+    INTERNAL_NODE_NUM_KEYS_OFFSET + INTERNAL_NODE_NUM_KEYS_SIZE;
+constexpr size_t INTERNAL_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE +
+                                             INTERNAL_NODE_NUM_KEYS_SIZE +
+                                             INTERNAL_NODE_RIGHT_CHILD_SIZE;
 
 constexpr size_t INTERNAL_NODE_KEY_SIZE = sizeof(uint32_t);
 constexpr size_t INTERNAL_NODE_CHILD_SIZE = sizeof(uint32_t);
-constexpr size_t INTERNAL_NODE_CELL_SIZE = INTERNAL_NODE_KEY_SIZE + INTERNAL_NODE_CHILD_SIZE;
+constexpr size_t INTERNAL_NODE_CELL_SIZE =
+    INTERNAL_NODE_KEY_SIZE + INTERNAL_NODE_CHILD_SIZE;
 constexpr size_t INTERNAL_NODE_MAX_KEYS = 510;
 
 #endif
